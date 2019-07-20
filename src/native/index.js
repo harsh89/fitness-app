99b42f3f62bcd888244e5434768ed8dec4871ec9
Harsh Gupta <harshgupta9@deloitte.com>
2019-07-20 17:20:32 +0530

    - merged notification code

--- src/native/index.js
+++ src/native/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, Platform } from 'react-native';
+import { StatusBar, Platform, Alert } from 'react-native';
 import { Font } from 'expo';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
@@ -13,9 +13,20 @@ import theme from '../../native-base-theme/variables/commonColor';
 
 import { LoggedInUserRoutes, guestUserRoutes } from './routes/index';
 import Loading from './components/UI/Loading';
+import { Constants, Notifications, Permissions } from 'expo';
 
 // Hide StatusBar on Android as it overlaps tabs
 if (Platform.OS === 'android') StatusBar.setHidden(true);
+
+async function getiOSNotificationPermission() {
+  const { status } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  if (status !== 'granted') {
+    await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  }
+}
+
 
 export default class App extends React.Component {
   static propTypes = {
@@ -23,9 +34,44 @@ export default class App extends React.Component {
     persistor: PropTypes.shape({}).isRequired,
   }
 
-  state = { loading: true, loggedIn: false }
+  state = {
+    loading: true,
+    loggedIn: false,
+    token: '',
+    error: ''
+  }
+
+  _handleButtonPress = () => {
+    const localnotification = {
+      title: 'Drink Water',
+      body: 'keep yourself Hyderated',
+      android: {
+        sound: true,
+      },
+      ios: {
+        sound: true,
+      },
+    };
+    let sendAfterFiveSeconds = Date.now();
+    sendAfterFiveSeconds += 5000;
+
+    const schedulingOptions = { time: sendAfterFiveSeconds, repeat: 'minute' };
+    Notifications.scheduleLocalNotificationAsync(
+      localnotification,
+      schedulingOptions
+    );
+  };
+  listenForNotifications = () => {
+    Notifications.addListener(notification => {
+      if (notification.origin === 'received' && Platform.OS === 'ios') {
+        Alert.alert(notification.title, notification.body);
+      }
+    });
+  };
 
   async componentWillMount() {
+    getiOSNotificationPermission();
+    this.listenForNotifications();
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
@@ -60,6 +106,7 @@ export default class App extends React.Component {
   render() {
     const { loading } = this.state;
     const { store, persistor } = this.props;
+    this._handleButtonPress();
 
     if (loading) return <Loading />;
 
